6413280912ae6b74bce1100a1e1d623b4277a7ef
TheLDB <ldb@erikboles.com>
2022-08-11 09:01:43 -0600

    added an increment'r and fixed some rounding issues

--- src/helpers/onMessage.ts
+++ src/helpers/onMessage.ts
@@ -1,71 +1,68 @@
 import { Client } from "discord.js";
 import { PrismaClient } from "@prisma/client";
 const onMessage = (client: Client): void => {
-    const prisma = new PrismaClient();
-    client.on("messageCreate", async (message) => {
-        if(message.author.bot) return;
+	const prisma = new PrismaClient();
+	client.on("messageCreate", async (message) => {
+		if (message.author.bot) return;
 
-        // * Get channel ID
-        const channelID = message.channelId;
+		// * Get channel ID
+		const channelID = message.channelId;
 
-        // * Check if channelID is registered in Supabase
+		// * Check if channelID is registered in Supabase
 
-        let channelExists = await prisma.countStatus.findUnique({
-            where: {
-                channelID
-            }
-        });
+		let channelExists = await prisma.countStatus.findUnique({
+			where: {
+				channelID,
+			},
+		});
 
-        // * If the channel is a counting channel, and the message content can be converted into a number
-        if(channelExists && !Number.isNaN(Number(message.content))) {
-            const sentNumber = Number(message.content);
-            if(sentNumber - Number(channelExists.currentNum) === 1) {
-                // * Number is 1 higher, increment and react
-                if(sentNumber > Number(channelExists.highestStreak)) {
-                    // * If it's a new high streak, update the score
-                    message.react('✅')
-                    
-                    await prisma.countStatus.update({
-                        where: {
-                            channelID
-                        },
-                        data: {
-                            currentNum: sentNumber,
-                            highestStreak: sentNumber,
-                        }
-                    })
-                }
-                else {
-                    // * Else, just update the currentNum
-                    message.react('✅')
+		// * If the channel is a counting channel, and the message content can be converted into a number
+		if (channelExists && !Number.isNaN(Number(message.content))) {
+			const sentNumber = Number(message.content);
+			
+			if (Math.round(100 * (sentNumber - Number(channelExists.currentNum))) / 100 === Number(channelExists.increment)) {
+				// * Number is correct, increment and react
+				if (sentNumber > Number(channelExists.highestStreak)) {
+					// * If it's a new high streak, update the score
+					message.react("✅");
 
-                    await prisma.countStatus.update({
-                        where: {
-                            channelID
-                        },
-                        data: {
-                            currentNum: sentNumber
-                        }
-                    });
-                }
-                
-            }
-            else {
-                // * Number is wrong, set counter to 0 and react with an X
-                await prisma.countStatus.update({
-                    where: {
-                        channelID
-                    },
-                    data: {
-                        currentNum: 0
-                    }
-                });
+					await prisma.countStatus.update({
+						where: {
+							channelID,
+						},
+						data: {
+							currentNum: sentNumber,
+							highestStreak: sentNumber,
+						},
+					});
+				} else {
+					// * Else, just update the currentNum
+					message.react("✅");
 
-                message.react('❌')
-            }
-        }
-        
-    })
-}
+					await prisma.countStatus.update({
+						where: {
+							channelID,
+						},
+						data: {
+							currentNum: sentNumber,
+						},
+					});
+				}
+			} else {
+				// * Number is wrong, set counter to 0 and react with an X
+				await prisma.countStatus.update({
+					where: {
+						channelID,
+					},
+					data: {
+						currentNum: 0,
+					},
+				});
 
-export { onMessage }
+				message.react("❌");
+			}
+		}
+	});
+};
+
+export { onMessage };
